39c24c5e331310e97c7924a72feff9f16d48e19c
Mateusz Michalczyk <pan.michalczyk@gmail.com>
2019-02-20 09:44:45 +0100

    Add heading-one, box, carousel style. Add slick-theme stylesheet, JS files to HTML. Add HEADING ONE section to HTML. Add graphics

---
+++ js/script.js
@@ -0,0 +1,25 @@
+$(document).on('ready', function() {
+  $('.responsive').slick({
+      dots: false,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      responsive: [
+        {
+          breakpoint: 991,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1
+          }
+        },
+        {
+          breakpoint: 767,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+      ]
+    });
+});
